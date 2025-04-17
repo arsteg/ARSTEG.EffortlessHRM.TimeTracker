de0f38ae5f67bc7c26bdd4ca7bbe7eed85965c42
ARSTEG SOLUTIONS <info@arsteg.com>
2025-04-17 16:44:44 +0530

    loaded the time tracker window, fixed UI errors

--- TimeTrackerX/Views/TimeTrackerView.axaml.cs
+++ TimeTrackerX/Views/TimeTrackerView.axaml.cs
@@ -1,10 +1,13 @@
 using System;
+using System.ComponentModel;
 using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Notifications;
 using Avalonia.Input;
 using Avalonia.Interactivity;
+using Avalonia.Media.Imaging;
+using DocumentFormat.OpenXml.Drawing.Charts;
 using Microsoft.Extensions.DependencyInjection;
 using TimeTrackerX.Services;
 using TimeTrackerX.ViewModels;
@@ -38,89 +41,123 @@ namespace TimeTrackerX.Views
             };
         }
 
-        //private SystemTrayIcon SetupSystemTray()
+        //private MsBox.Avalonia.Enums.Icon ConvertImageToIcon(string imagePath)
         //{
-        //    var trayIcon = new SystemTrayIcon
+        //    using (Bitmap bitmap = new Bitmap(imagePath))
         //    {
-        //        Icon = new WindowIcon("Assets/smallLogo.png"),
-        //        ToolTipText = "EffortlessHRM - Time Tracker"
-        //    };
-        //    trayIcon.Clicked += (s, e) =>
-        //    {
-        //        Show();
-        //        WindowState = WindowState.Normal;
-        //        Activate();
-        //        Focus();
-        //    };
-        //    SystemTray.SetSystemTrayIcon(this, trayIcon);
-        //    return trayIcon;
+        //        IntPtr hIcon = bitmap.GetHicon();
+        //        return System.Drawing.Icon.FromHandle(hIcon);
+        //    }
         //}
 
-        private void BtnActionMinimize_OnPointerPressed(object? sender, PointerPressedEventArgs e)
+        //protected override void OnStateChanged(EventArgs e)
+        //{
+        //    if (WindowState == WindowState.Minimized)
+        //    {
+        //        this.Hide();
+        //    }
+        //    base.OnStateChanged(e);
+        //    UpdatePopupPosition();
+        //}
+
+        // Minimize to system tray when application is closed.
+        //protected override void OnClosing(CancelEventArgs e)
+        //{
+        //    // setting cancel to true will cancel the close request
+        //    // so the application is not closed
+        //    e.Cancel = true;
+
+        //    this.Hide();
+
+        //    base.OnClosing(e);
+        //}
+
+        private void UpdatePopupPosition()
         {
-            if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
-            {
-                WindowState = WindowState.Minimized;
-                Hide();
-            }
+            //var screenWidth = SystemParameters.WorkArea.Width;
+            //var screenHeight = SystemParameters.WorkArea.Height;
+            //var popupWidth = popupControl.ActualWidth;
+            //var popupHeight = popupControl.ActualHeight;
+
+            //popupControl.PlacementRectangle = new Rect(
+            //    screenWidth + 140,
+            //    screenHeight - popupHeight,
+            //    popupWidth,
+            //    popupHeight
+            //);
         }
 
-        private void BtnActionClose_OnPointerPressed(object? sender, PointerPressedEventArgs e)
+        //private void Thumb_OnDragDelta(object sender, DragDeltaEventArgs e)
+        //{
+        //    Left = Left + e.HorizontalChange;
+        //    Top = Top + e.VerticalChange;
+        //}
+
+        private void btnActionMinimize_Click(object sender, RoutedEventArgs e)
         {
-            if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
-            {
-                Hide();
-            }
+            this.WindowState = WindowState.Minimized;
         }
 
-        private void Border_OnPointerPressed(object? sender, PointerPressedEventArgs e)
+        //private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        //{
+        //    if (e.ChangedButton == MouseButton.Left)
+        //    {
+        //        //this.DragMove();
+        //    }
+        //}
+
+        private void BtnActionMinimize_OnClick(object sender, RoutedEventArgs e)
         {
-            if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
-            {
-                BeginMoveDrag(e);
-            }
+            WindowState = WindowState.Minimized;
         }
 
-        private void LoggingToggle_Checked(object? sender, RoutedEventArgs e)
+        private void btnActionClose_Click(object sender, RoutedEventArgs e)
+        {
+            //Application.Current.Shutdown();
+        }
+
+        //private void btnActionClose_Click(object sender, MouseButtonEventArgs e)
+        //{
+        //    //Application.Current.Shutdown();
+        //}
+
+        public void RestoreWindow()
+        {
+            if (this.WindowState == WindowState.Minimized)
+            {
+                this.Show();
+                this.WindowState = WindowState.Normal;
+            }
+            this.Activate();
+            this.Focus();
+        }
+
+        private void LoggingToggle_Checked(object sender, RoutedEventArgs e)
         {
             if (sender is CheckBox checkBox && checkBox.Tag is string logLevel)
             {
-                Settings.Default.SetLoggingEnabled(logLevel, true);
-                //_notificationService.Show(
-                //    $"Logging {logLevel} Enabled",
-                //    $"Logging for {logLevel} has been enabled."
-                //);
+                //LogManager.SetLoggingEnabled(logLevel, true);
             }
         }
 
-        private void LoggingToggle_Unchecked(object? sender, RoutedEventArgs e)
+        private void LoggingToggle_Unchecked(object sender, RoutedEventArgs e)
         {
             if (sender is CheckBox checkBox && checkBox.Tag is string logLevel)
             {
-                Settings.Default.SetLoggingEnabled(logLevel, false);
-                //_notificationService.Show(
-                //    $"Logging {logLevel} Disabled",
-                //    $"Logging for {logLevel} has been disabled."
-                //);
+                //LogManager.SetLoggingEnabled(logLevel, false);
             }
         }
 
         private void LoadUserSettings()
         {
-            //var settings = Settings.Default;
-            //var vm = (TimeTrackerViewModel)DataContext;
-            //vm.EnableInfoLogging = settings.EnableInfoLogging;
-            //vm.EnableWarnLogging = settings.EnableWarnLogging;
-            //vm.EnableErrorLogging = settings.EnableErrorLogging;
-        }
+            //LogManager.SetLoggingEnabled("Info", Properties.Settings.Default.EnableInfoLogging);
+            //LogManager.SetLoggingEnabled("Warn", Properties.Settings.Default.EnableWarnLogging);
+            //LogManager.SetLoggingEnabled("Error", Properties.Settings.Default.EnableErrorLogging);
 
-        protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
-        {
-            base.OnPropertyChanged(change);
-            if (change.Property == WindowStateProperty && WindowState == WindowState.Minimized)
-            {
-                Hide();
-            }
+            //// Update UI
+            //InfoCheckBox.IsChecked = Properties.Settings.Default.EnableInfoLogging;
+            //WarnCheckBox.IsChecked = Properties.Settings.Default.EnableWarnLogging;
+            //ErrorCheckBox.IsChecked = Properties.Settings.Default.EnableErrorLogging;
         }
     }
 
